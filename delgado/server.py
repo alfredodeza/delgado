222ec5c282c777e3ba050f0f70e2250ae86d915d
Alfredo Deza <alfredodeza@gmail.com>
2013-03-11 08:23:13 -0400

    cleaner implementation of the server

--- delgado/server.py
+++ delgado/server.py
@@ -25,36 +25,37 @@ Run the server that listens on a unix socket.
         parser.catch_help = self._help
         parser.parse_args()
         delgado.config['allowed'] = parser.get('--allowed', [])
-        self.run()
+        while True:
+            try:
+                logger.debug('creating a new connection')
+                self.run()
+            except Reconnect:
+                pass
 
-    def run(self):
+    def connection(self):
         s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         try:
             os.remove("/tmp/socketname")
         except OSError:
             pass
+        s.bind("/tmp/socketname")
+        s.listen(1)
+        conn, addr = s.accept()
+        logger.debug('connection ready and listenting')
+        return conn
+
+    def run(self):
+        conn = self.connection()
+        raw_data = conn.recv(1024)
         try:
-            while True:
-                try:
-                    s.bind("/tmp/socketname")
-                    s.listen(1)
-                    conn, addr = s.accept()
-                except socket.error:
-                    raise Reconnect
-                while 1:
-                    raw_data = conn.recv(1024)
-                    try:
-                        if not raw_data:
-                            raise Reconnect
-                        data = loader(raw_data)
-                        logger.info("Running command: %s" % data)
-                        call(data)
-                    except (InvalidFormat, Forbidden) as error:
-                        logger.error(error.message)
-                        raise Reconnect
-        except Reconnect:
-            try:
-                conn.close()
-            except:
-                pass
-            self.run()
+            if not raw_data:
+                raise Reconnect
+            data = loader(raw_data)
+            logger.info("Running command: %s" % data)
+            call(data)
+        except (InvalidFormat, Forbidden) as error:
+            logger.error(error.message)
+            raise Reconnect
+        finally:
+            conn.close()
+            logger.debug('connection closed')
