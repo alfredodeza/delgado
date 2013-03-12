7847d8a15cf2e9df4c08744d656955aec893d828
Alfredo Deza <alfredodeza@gmail.com>
2013-03-11 22:54:50 -0400

    working py.test plugin

--- delgado/services/pytest.py
+++ delgado/services/pytest.py
@@ -1,13 +1,26 @@
+from tambo import Transport
+import delgado
+from delgado.server import Engine
 
 
 class Pytest(object):
 
     help_menu = 'A handler for running py.test commands'
+    _help = """
+Run a base socket listener that allows py.test commands.
 
+--socket-location   The location for the socket (defaults
+                    to /tmp/pytest.sock)
+    """
 
-    def __init__(self, args):
-        self.args = args
-        print 'initing Pytest plugin'
+    def __init__(self, argv):
+        self.argv = argv
 
     def parse_args(self):
-        print 'parsing args'
+        parser = Transport(self.argv, options=['--socket-location'])
+        parser.catch_help = self._help
+        parser.parse_args()
+        location = parser.get('--socket-location', '/tmp/pytest.sock')
+        delgado.config['allowed'] = ['py.test']
+        engine = Engine(socket_location=location)
+        engine.run_forever()
